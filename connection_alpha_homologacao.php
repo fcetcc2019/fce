481ef2f6c9c71f9990e9f49aa2924819de7b2006
Demétrius Jr <15demi08@users.noreply.github.com>
2019-10-01 21:59:54 -0300

    Update connection_alpha_homologacao.php

--- connection_alpha_homologacao.php
+++ connection_alpha_homologacao.php
@@ -6,7 +6,8 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-$db_alpha = new mysqli($server, $username, $password, $db);
+//$db_alpha = new mysqli($server, $username, $password, $db);
+$db_alpha = mysqli_connect($server, $username, $password, $db);
 
 echo $server.'\n';
 echo $username.'\n';
