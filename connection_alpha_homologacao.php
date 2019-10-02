bb9d44f9a9c78b21ea5be35fdf3c03184e604742
Demétrius Jr <15demi08@users.noreply.github.com>
2019-10-01 21:57:10 -0300

    trocado mysql por mysqli

--- connection_alpha_homologacao.php
+++ connection_alpha_homologacao.php
@@ -1,6 +1,4 @@
 <?php
-ini_set('error_reporting',E_ALL);
-
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["host"];
@@ -8,8 +6,7 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-$db_alpha = mysql_connect($server, $username, $password) or die ("DB error!".mysql_error());//, $db);
-mysql_select_db($db, $db_alpha) or die ("Erro!");
+$db_alpha = new mysqli($server, $username, $password, $db);
 
 echo $server.'\n';
 echo $username.'\n';
