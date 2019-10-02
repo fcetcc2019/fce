04a5d912b4593486b3752f3824b045e5fe5b3151
Demétrius Jr <15demi08@users.noreply.github.com>
2019-10-01 21:21:16 -0300

    Update connection_alpha_homologacao.php
    Colocando as informações do Heroku nos parâmetros de conexão com o banco

--- connection_alpha_homologacao.php
+++ connection_alpha_homologacao.php
@@ -2,22 +2,16 @@
 
   $writerUser = array('user' => 'PortalWriter', 'pass' => 'P0rt@1w81terR$');
   $readerUser = array('user' => 'PortalReader', 'pass' => 'P0rt@183@d3rR$');
-  $defaultCatalog = "PortalSenacRS";
-  
-  
-  //$db = mysql_connect("ALPHA\\PORTAL", $writerUser['user'], $writerUser['pass']) or die ("DB error!".mysql_get_last_message());
-  //$catalog = mysql_select_db($defaultCatalog, $db) or die ("Erro!");
+  //$defaultCatalog = "PortalSenacRS";
+  $defaultCatalog = "heroku_035be66877c33ed";
+    
+  //$db_alpha = mysql_connect("localhost:3308", "root", "") or die ("DB error!".mysql_error());
+  $db_alpha = mysql_connect("us-cdbr-iron-east-02.cleardb.net", "b418da099072c8", "919d9ec7") or die ("DB error!".mysql_error());
 
-  
-  $db_alpha = mysql_connect("localhost:3308", "root", "") or die ("DB error!".mysql_error());
+
   //echo "Conectou ao banco corretamente.<br />";
   $catalog_alpha = mysql_select_db($defaultCatalog, $db_alpha) or die ("Erro!");
   //echo "Selecionou corretamente ".$defaultCatalog.".<br />";
 
-  
-  //$db_alpha_homologacao = mysql_connect("ALPHA\\HOMOLOGACAO", "intranet", "!ntr@net") or die ("DB error!".mysql_get_last_message());
-  //echo "Conectou ao banco corretamente.<br />";
-  //$catalog_alpha_homologacao = mysql_select_db("PortalSenacRS_Homologacao", $db_alpha_homologacao) or die ("Erro!");
-  //echo "Selecionou corretamente ".$defaultCatalog.".<br />";
 
 ?>
