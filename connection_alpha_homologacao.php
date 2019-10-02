65c9308b62147038c6190e76f1f030b3e6ecce47
Demétrius Jr <15demi08@users.noreply.github.com>
2019-10-01 21:46:35 -0300

    Update connection_alpha_homologacao.php

--- connection_alpha_homologacao.php
+++ connection_alpha_homologacao.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('error_reporting',E_ALL);
+
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["host"];
@@ -6,7 +8,7 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-$db_alpha = mysql_connect($server, $username, $password);//, $db);
+$db_alpha = mysql_connect($server, $username, $password) or die ("DB error!".mysql_error());//, $db);
 mysql_select_db($db, $db_alpha) or die ("Erro!");
 
 echo $server.'\n';
