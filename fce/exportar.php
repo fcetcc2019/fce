008ded6191eceb10727d8102ff411ee877cea34a
Demétrius Jr <15demi08@users.noreply.github.com>
2019-11-18 23:26:00 -0300

    Update exportar.php
    Viajei XD

--- fce/exportar.php
+++ fce/exportar.php
@@ -27,7 +27,7 @@ if(isset($_GET['id']) && !empty($_GET['id'])) {
 	// Todas as respostas
 	//$resultado = mysqli_fetch_all($query, MYSQLI_FETCH_ASSOC);
 
-	while($resultado = mysqli_fetch_assoc)
+	while($resultado = mysqli_fetch_assoc($query))
 		var_dump($resultado);
 
 	die();
